568dba5f6a7ff2d73f9823a2473efe7afb159e97
Vannevelj <jer_vannevel@hotmail.com>
2015-03-20 20:53:10 +0100

    Added OAuth authorization provider

---
+++ MoviePicker/WebApi/ApiModels/Authentication/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Database.Repositories.Declarations;
+using Microsoft.Owin.Security.OAuth;
+
+#pragma warning disable 1998
+
+namespace WebApi.ApiModels.Authentication
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider, IDisposable
+    {
+        private readonly IUserRepository _userRepository;
+
+        public SimpleAuthorizationServerProvider(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void Dispose()
+        {
+            _userRepository.Dispose();
+        }
+
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] {"*"});
+
+            var user = _userRepository.FindUser(context.UserName, context.Password);
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "The username or password is incorrect.");
+                return;
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+            context.Validated();
+        }
+    }
+}
--- MoviePicker/WebApi/App_Start/Startup.cs
+++ MoviePicker/WebApi/App_Start/Startup.cs
@@ -1,15 +1,38 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
+using Database.Repositories.Declarations;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using WebApi.ApiModels.Authentication;
 
 namespace WebApi.App_Start
 {
     public class Startup
     {
+        private HttpConfiguration _configuration;
+
         public void Configuration(IAppBuilder app)
         {
+            ConfigureOAuth(app);
             var config = new HttpConfiguration();
             WebApiConfig.Register(config);
             app.UseWebApi(config);
+            _configuration = config;
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            var oAuthServerOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider((IUserRepository) _configuration.DependencyResolver.GetService(typeof (IUserRepository)))
+            };
+
+            app.UseOAuthAuthorizationServer(oAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
         }
     }
 }

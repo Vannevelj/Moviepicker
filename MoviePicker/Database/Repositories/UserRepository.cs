1506b87d813df8a9ce649c9d3d2e454b4aff1068
Vannevelj <jer_vannevel@hotmail.com>
2015-03-22 02:05:11 +0100

    Added new repository methods for refresh tokens and client apps

--- MoviePicker/Database/Repositories/UserRepository.cs
+++ MoviePicker/Database/Repositories/UserRepository.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Database.DatabaseModels;
 using Database.Repositories.Declarations;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Models.Users;
+using Models.Users.Authorization;
 
 namespace Database.Repositories
 {
@@ -27,6 +30,53 @@ namespace Database.Repositories
         public async Task<IdentityUser> FindUserAsync(string username, string password)
         {
             return await _userManager.FindAsync(username, password);
+        }
+
+        public async Task<ClientApplication> FindClientAsync(string clientId)
+        {
+            return await _context.ClientApplications.FindAsync(clientId);
+        }
+
+        public async Task<bool> TryAddRefreshTokenAsync(RefreshToken refreshToken)
+        {
+            var existingToken = _context.RefreshTokens.SingleOrDefault(x => x.Subject == refreshToken.Subject && x.ClientApplicationId == refreshToken.ClientApplicationId);
+
+            if (existingToken != null)
+            {
+                await TryRemoveRefreshTokenAsync(existingToken);
+            }
+
+            _context.RefreshTokens.Add(refreshToken);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> TryRemoveRefreshTokenAsync(string refreshTokenId)
+        {
+            var existingToken = await _context.RefreshTokens.FindAsync(refreshTokenId);
+
+            if (existingToken != null)
+            {
+                _context.RefreshTokens.Remove(existingToken);
+                return await _context.SaveChangesAsync() > 0;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> TryRemoveRefreshTokenAsync(RefreshToken refreshToken)
+        {
+            _context.RefreshTokens.Remove(refreshToken);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<RefreshToken> FindRefreshTokenAsync(string refreshTokenId)
+        {
+            return await _context.RefreshTokens.FindAsync(refreshTokenId);
+        }
+
+        public IEnumerable<RefreshToken> GetRefreshTokens()
+        {
+            return _context.RefreshTokens;
         }
     }
 }

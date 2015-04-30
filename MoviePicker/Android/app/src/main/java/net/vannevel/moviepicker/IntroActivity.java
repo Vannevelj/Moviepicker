97575b1b5717f2cddd29c57b4eab6dfb590ec87d
Vannevelj <jer_vannevel@hotmail.com>
2015-04-30 23:47:37 +0200

    Using string resources, removed loginfragment, cleaned up login activity

--- MoviePicker/Android/app/src/main/java/net/vannevel/moviepicker/IntroActivity.java
+++ MoviePicker/Android/app/src/main/java/net/vannevel/moviepicker/IntroActivity.java
@@ -1,30 +1,18 @@
 package net.vannevel.moviepicker;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 
 public class IntroActivity extends ActionBarActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,19 +36,11 @@ public class IntroActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    public void onLoginButtonClicked(View view) {
+        // try login
+    }
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_login, container, false);
-            return rootView;
-        }
+    public void onRegistrationButtonClicked(View view) {
+        // go to registration page
     }
 }
